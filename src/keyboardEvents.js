e2f236610f39270c8f760607a64f56acff99cda5
euhoo <euhoo@mail.ru>
2019-03-23 15:13:24 +0300

    fix wrong key error

--- src/keyboardEvents.js
+++ src/keyboardEvents.js
@@ -7,22 +7,24 @@ const coordinateChange = {
 
 export default (table) => {
   document.addEventListener('keyup', ({ key }) => {
-    const emptyCell = document.querySelector('.table-active');
-    const passive = {
-      row: emptyCell.closest('tr').rowIndex,
-      cell: emptyCell.cellIndex,
-    };
-    const active = {
-      row: passive.row + coordinateChange[key][0],
-      cell: passive.cell + coordinateChange[key][1],
-    };
-    Object.keys(active).forEach((el) => {
-      if (active[el] > 3) active[el] = 3;
-      if (active[el] < 0) active[el] = 0;
-    });
-    const numCell = table.rows[active.row].cells[active.cell];
-    emptyCell.classList.remove('table-active');
-    numCell.classList.add('table-active');
-    [emptyCell.textContent, numCell.textContent] = [numCell.textContent, emptyCell.textContent];
+    if (key in coordinateChange) {
+      const emptyCell = document.querySelector('.table-active');
+      const passive = {
+        row: emptyCell.closest('tr').rowIndex,
+        cell: emptyCell.cellIndex,
+      };
+      const active = {
+        row: passive.row + coordinateChange[key][0],
+        cell: passive.cell + coordinateChange[key][1],
+      };
+      Object.keys(active).forEach((el) => {
+        if (active[el] > 3) active[el] = 3;
+        if (active[el] < 0) active[el] = 0;
+      });
+      const numCell = table.rows[active.row].cells[active.cell];
+      emptyCell.classList.remove('table-active');
+      numCell.classList.add('table-active');
+      [emptyCell.textContent, numCell.textContent] = [numCell.textContent, emptyCell.textContent];
+    }
   });
 };
