6c24b27f0f413ac216363e5b80343ad3c3494994
euhoo <euhoo@mail.ru>
2019-03-23 19:29:04 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,6 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000,
+    stats: 'errors-only', // выводит в терминал только ошибки
   },
 };
